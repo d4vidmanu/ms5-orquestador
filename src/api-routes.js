f6a5e7bd6fabed8dea69363893e30d6997c2afe2
d4vidmanu <david.silva.m@utec.edu.pe>
2024-10-09 18:08:59 -0500

    docker network

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,17 +1,17 @@
-// const apiRoutes = {
-//   gestionUsuario: "http://gestion_usuario:8000",
-//   scooterManagement: "http://ms2_scooter_management:3000",
-//   transaction: "http://ms3_transaction:3010",
-//   maintenance: "http://ms4_maintenance:3003",
-// };
-
-// export default apiRoutes;
-
 const apiRoutes = {
-  gestionUsuario: "http://localhost:3001",
-  scooterManagement: "http://localhost:3000",
-  transaction: "http://localhost:3010",
-  maintenance: "http://localhost:3003",
+  gestionUsuario: "http://gestion_usuario:8000",
+  scooterManagement: "http://ms2_scooter_management:3000",
+  transaction: "http://ms3_transaction:3010",
+  maintenance: "http://ms4_maintenance:3003",
 };
 
 export default apiRoutes;
+
+// const apiRoutes = {
+//   gestionUsuario: "http://localhost:3001",
+//   scooterManagement: "http://localhost:3000",
+//   transaction: "http://localhost:3010",
+//   maintenance: "http://localhost:3003",
+// };
+
+// export default apiRoutes;
